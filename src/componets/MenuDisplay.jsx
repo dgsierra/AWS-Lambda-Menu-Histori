e3c6f8440cd8f9c3961003917386a7be29b7ab13
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-03-02 22:13:17 -0600

    Add PUT functionality

--- src/componets/MenuDisplay.jsx
+++ src/componets/MenuDisplay.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import { useGetMenuQuery } from '../redux/menu'
+import { useGetMenuQuery, usePostMenuMutation } from '../redux/menu'
 
 export const MenuDisplay = () => {
-  return (<h1> Hola </h1>)
+  const { data, isLoading } = useGetMenuQuery()
+  const [postMenu, result] = usePostMenuMutation()
+  console.log(data)
+  console.log(isLoading)
+  const postHandler = () => {
+    alert('postHandler')
+    postMenu({
+      dish: 'Mole',
+      role: 'manager',
+      img: 'enchiladas img'
+    })
+  }
+  return (
+  <div>
+    <h1> Hola </h1>
+    <button onClick={postHandler}></button>
+  </div>
+  )
 }
 
 export default MenuDisplay
--- src/redux/menu.jsx
+++ src/redux/menu.jsx
@@ -1,15 +1,34 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const MENU_API = 'https://xbgrnp37ak.execute-api.us-east-2.amazonaws.com/items'
+const MENU_API = 'https://xbgrnp37ak.execute-api.us-east-2.amazonaws.com/'
 
 export const menuApi = createApi({
   reducerPath: 'menuApi',
-  baseQuery: fetchBaseQuery({ baseUrl: MENU_API }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: MENU_API,
+    prepareHeaders: (headers) => {
+      headers.set('Content-Type', 'application/json')
+      headers.set('Host', 'xbgrnp37ak.execute-api.us-east-2.amazonaws.com')
+      return headers
+    }
+  }),
   endpoints: (builder) => ({
     getMenu: builder.query({
       query: () => 'items'
+    }),
+    postMenu: builder.mutation({
+      query: (body) => ({
+        url: 'items',
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Host: 'xbgrnp37ak.execute-api.us-east-2.amazonaws.com',
+          accept: 'application/json'
+        },
+        body: body
+      })
     })
   })
 })
 
-export const { useGetMenuQuery } = menuApi
+export const { useGetMenuQuery, usePostMenuMutation } = menuApi
